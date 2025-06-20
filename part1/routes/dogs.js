72f991b4c2d9bda81b562d8b2c35bf26e59df4be
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-20 09:19:50 +0000

    [automatic commit] 2025-06-20 09:19:50 UTC 139.218.234.143

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -3,26 +3,16 @@ const router = express.Router();
 const db = require('../db');
 
 router.get('/dogs', async(req, res) => {
+    try {
     const [rows]= await db.query(`SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
          FROM Dogs JOIN Users ON Dogs.owner_id = Users.user_id WHERE Users.role = 'owner';
 `);
     res.json(rows);
+    }catch (err) {
+    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
+  }
 });
 
 
-let dbConnectionPool;
-  try {
-    // Connect to MySQL without specifying a database
-      dbConnectionPool = mysql.createPool({
-socketPath: '/var/run/mysqld/mysqld.sock',
-host: 'localhost',
-user: 'root',
-password: '',
-database: 'DogWalkService'
-    });
-
-  } catch (err) {
-    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
-  }
 
 module.exports = router;
