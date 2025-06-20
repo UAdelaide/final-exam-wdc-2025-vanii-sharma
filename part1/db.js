fab64e2b2d60fffadf71706cf7bf214fa8a610a7
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-20 08:46:32 +0000

    [automatic commit] 2025-06-20 08:46:32 UTC 139.218.234.143

--- part1/db.js
+++ part1/db.js
@@ -12,9 +12,12 @@ var mysql = require('mysql2/promise');
 let dbConnectionPool;
   try {
     // Connect to MySQL without specifying a database
-    const connection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
+    dbConnectionPool = mysql.createPool({
+// socketPath: '/var/run/mysqld/mysqld.sock',
+// host: '127.0.0.1',
+// user: 'root',
+// password: 'password',
+// database: 'DogWalkService'
       password: '' // Set your MySQL root password
     });
     // Now connect to the created database
