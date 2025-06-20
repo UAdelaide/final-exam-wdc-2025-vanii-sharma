f42934d0245866fb6bd24c89b55159ebaccae96f
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-20 09:26:58 +0000

    [automatic commit] 2025-06-20 09:26:58 UTC 139.218.234.143

--- part1/app.js
+++ part1/app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dogsRouter = require('./routes/dogs');
 var walkRequestsOpenRouter = require('./routes.walkrequestsOpen');
-var walkersSummaryRouter = require('./routes')
+var walkersSummaryRouter = require('./routes.walkersSummary')
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api', dogsRouter);
+app.use('/api', dogsRouter, );
 
 
 module.exports = app;
