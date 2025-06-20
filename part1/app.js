0bff024540b25803dbfe1a55bc27c20bd28015e9
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-20 08:29:52 +0000

    [automatic commit] 2025-06-20 08:29:52 UTC 139.218.234.143

--- part1/app.js
+++ part1/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var dogsRouter = require('./api/dogs');
+var dogsRouter = require('./routes/dogs');
 
 var app = express();
 
