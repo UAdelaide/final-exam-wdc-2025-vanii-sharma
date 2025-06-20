3119fe65ef40dd3aca7306dc3604b0718db71ac2
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-20 08:41:45 +0000

    [automatic commit] 2025-06-20 08:41:45 UTC 139.218.234.143

--- part1/app.js
+++ part1/app.js
@@ -73,13 +73,5 @@ app.use('/users', usersRouter);
 app.use('/api', dogsRouter);
 
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-
 module.exports = app;
 
