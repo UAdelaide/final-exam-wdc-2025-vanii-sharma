831b0884594585ea13978fed877e6d850f158ac3
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-20 09:57:32 +0000

    [automatic commit] 2025-06-20 09:57:32 UTC 139.218.234.143

--- part1/routes/walkrequestsOpen.js
+++ part1/routes/walkrequestsOpen.js
@@ -4,7 +4,13 @@ const db = require('../db');
 
 router.get('/open', async(req, res) => {
     try {
-    const [open]= await db.execute(`SELECT * FROM Dogs;
+    const [open]= await db.execute(`SELECT WalkRequests.request_id, Dogs.name AS dog_name,
+         WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.Location,
+          Users.username AS owner_username
+FROM WalkRequests
+JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
+JOIN Users ON Dogs.owner_id = Users.user_id
+WHERE WalkRequests.status = 'open';;
 `);
     res.status(200).json(open);
     }catch (err) {
