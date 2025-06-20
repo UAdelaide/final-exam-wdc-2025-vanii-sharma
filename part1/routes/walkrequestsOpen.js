c4828c97bb4ed289d3577b5daa9205bd6317e50d
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-20 09:41:14 +0000

    [automatic commit] 2025-06-20 09:41:14 UTC 139.218.234.143

--- part1/routes/walkrequestsOpen.js
+++ part1/routes/walkrequestsOpen.js
@@ -4,7 +4,7 @@ const db = require('../db');
 
 router.get('/open', async(req, res) => {
     try {
-    const [open]= await db.execute(`SELECT * FROM Users;
+    const [open]= await db.execute(`SELECT * FROM Dogs;
 `);
     res.status(200).json(open);
     }catch (err) {
