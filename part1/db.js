0b6e6b1de85afaca4e1898f0388cb725c4a99114
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-20 08:43:28 +0000

    [automatic commit] 2025-06-20 08:43:28 UTC 139.218.234.143

--- part1/db.js
+++ part1/db.js
@@ -1,12 +1,12 @@
 var mysql = require('mysql2/promise');
 
-var dbConnectionPool = mysql.createPool({
-socketPath: '/var/run/mysqld/mysqld.sock',
-host: '127.0.0.1',
-user: 'root',
-password: 'password',
-database: 'DogWalkService'
-});
+// var dbConnectionPool = mysql.createPool({
+// socketPath: '/var/run/mysqld/mysqld.sock',
+// host: '127.0.0.1',
+// user: 'root',
+// password: 'password',
+// database: 'DogWalkService'
+// });
 
 
 let db;
