07b92043ac1a0c9435250b08104b7c1c5f18bb15
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-20 17:22:47 +0000

    [automatic commit] 2025-06-20 17:22:47 UTC 139.218.234.143

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,7 +49,11 @@ router.post('/login', async (req, res) => {
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
-
+req.session.user = {
+      user_id: user.user_id,
+      username: user.username,
+      role: user.role
+    };
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
