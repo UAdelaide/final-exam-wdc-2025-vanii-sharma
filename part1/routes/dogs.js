086840bd69f1381698517e66655c11fec7618682
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-20 09:19:30 +0000

    [automatic commit] 2025-06-20 09:19:30 UTC 139.218.234.143

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -9,4 +9,20 @@ router.get('/dogs', async(req, res) => {
     res.json(rows);
 });
 
+
+let dbConnectionPool;
+  try {
+    // Connect to MySQL without specifying a database
+      dbConnectionPool = mysql.createPool({
+socketPath: '/var/run/mysqld/mysqld.sock',
+host: 'localhost',
+user: 'root',
+password: '',
+database: 'DogWalkService'
+    });
+
+  } catch (err) {
+    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
+  }
+
 module.exports = router;
