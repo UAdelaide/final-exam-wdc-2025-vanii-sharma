55d921c7dc22a3cc3969f58e5f7a7d58b3ff75ff
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-20 09:14:24 +0000

    [automatic commit] 2025-06-20 09:14:24 UTC 139.218.234.143

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../db');
 
 router.get('/dogs', async(req, res) => {
-    const [rows]= await db.query(`SELECT Dogs.name, Dogs.size, Dogs.owner_id FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE Users.role = 'owner';
+    const [rows]= await db.query(`SELECT Dogs.name AS dog_name, Dogs.size, Dogs.owner_id FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE Users.role = 'owner';
 `);
     res.json(rows);
 });
