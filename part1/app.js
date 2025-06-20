220d5ab0c321dd837e4e12a0d2b81bc21fd0493d
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-20 08:40:44 +0000

    [automatic commit] 2025-06-20 08:40:44 UTC 139.218.234.143

--- part1/app.js
+++ part1/app.js
@@ -34,24 +34,24 @@ let db;
     });
 
     // Create a table if it doesn't exist
-    await db.execute(`
-      CREATE TABLE IF NOT EXISTS books (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        title VARCHAR(255),
-        author VARCHAR(255)
-      )
-    `);
+    // await db.execute(`
+    //   CREATE TABLE IF NOT EXISTS books (
+    //     id INT AUTO_INCREMENT PRIMARY KEY,
+    //     title VARCHAR(255),
+    //     author VARCHAR(255)
+    //   )
+    // `);
 
-    // Insert data if table is empty
-    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
-    if (rows[0].count === 0) {
-      await db.execute(`
-        INSERT INTO books (title, author) VALUES
-        ('1984', 'George Orwell'),
-        ('To Kill a Mockingbird', 'Harper Lee'),
-        ('Brave New World', 'Aldous Huxley')
-      `);
-    }
+    // // Insert data if table is empty
+    // const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
+    // if (rows[0].count === 0) {
+    //   await db.execute(`
+    //     INSERT INTO books (title, author) VALUES
+    //     ('1984', 'George Orwell'),
+    //     ('To Kill a Mockingbird', 'Harper Lee'),
+    //     ('Brave New World', 'Aldous Huxley')
+    //   `);
+    // }
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
   }
