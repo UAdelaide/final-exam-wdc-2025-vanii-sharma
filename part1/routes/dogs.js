4b298bae0fee283182fa648a46f07fd33e31588d
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-20 09:23:55 +0000

    [automatic commit] 2025-06-20 09:23:55 UTC 139.218.234.143

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -7,7 +7,7 @@ router.get('/dogs', async(req, res) => {
     const [dogs]= await db.execute(`SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
          FROM Dogs JOIN Users ON Dogs.owner_id = Users.user_id WHERE Users.role = 'owner';
 `);
-    res.json(dogs);
+    res.status(200).json(dogs);
     }catch (err) {
     res.status(500).json({ error: 'Failed to fetch dogs' });
   }
