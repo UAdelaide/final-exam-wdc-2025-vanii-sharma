10e1a9f63b71328593ab6b5ebb284cd20c7d800f
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-20 10:25:24 +0000

    [automatic commit] 2025-06-20 10:25:24 UTC 139.218.234.143

--- part1/routes/walkersSummary.js
+++ part1/routes/walkersSummary.js
@@ -4,7 +4,21 @@ const db = require('../db');
 
 router.get('/summary', async(req, res) => {
     try {
-    const [summary]= await db.execute(`SELECT * FROM Users;
+    const [summary]= await db.execute(`SELECT
+    Users.username,
+    COUNT(WalkRatings.rating) AS total_ratings,
+    AVG(WalkRatings.rating) AS average_rating,
+    (
+        SELECT COUNT(*)
+        FROM WalkRequests
+        JOIN WalkApplications ON WalkRequests.request_id = WalkApplications.request_id
+        WHERE WalkRequests.status = 'completed' AND WalkApplications.walker_id = Users.user_id
+    ) AS completed_walks
+
+FROM Users
+LEFT JOIN WalkRatings ON Users.user_id = WalkRatings.walker_id
+WHERE Users.role = 'walker'
+GROUP BY Users.user_id;;
 `);
     res.status(200).json(summary);
     }catch (err) {
