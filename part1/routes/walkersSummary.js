67e77598e5c9ca30fb86091b49f6090d94181f8e
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-20 09:33:06 +0000

    [automatic commit] 2025-06-20 09:33:06 UTC 139.218.234.143

--- part1/routes/walkersSummary.js
+++ part1/routes/walkersSummary.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-router.get('/dogs', async(req, res) => {
+router.get('/walkers/summary', async(req, res) => {
     try {
-    const [dogs]= await db.execute(`SHOW DATABASES;
+    const [summary]= await db.execute(`SELECT * FROM Dogs;
 `);
     res.status(200).json(dogs);
     }catch (err) {
