505acb8f020bbe2832e826697ff80577a7390cbd
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-20 08:53:01 +0000

    [automatic commit] 2025-06-20 08:53:01 UTC 139.218.234.143

--- part1/db.js
+++ part1/db.js
@@ -1,14 +1,5 @@
 var mysql = require('mysql2/promise');
 
-// var dbConnectionPool = mysql.createPool({
-// socketPath: '/var/run/mysqld/mysqld.sock',
-// host: '127.0.0.1',
-// user: 'root',
-// password: 'password',
-// database: 'DogWalkService'
-// });
-
-
 let dbConnectionPool;
   try {
     // Connect to MySQL without specifying a database
@@ -19,13 +10,6 @@ user: 'root',
 password: '',
 database: 'DogWalkService'
     });
-    // // Now connect to the created database
-    // db = await mysql.createConnection({
-    //   host: 'localhost',
-    //   user: 'root',
-    //   password: '',
-    //   database: 'DogWalkService'
-    // });
 
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
