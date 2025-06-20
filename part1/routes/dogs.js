7aada922a3966590042772e3f032dc477269fa19
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-20 08:49:15 +0000

    [automatic commit] 2025-06-20 08:49:15 UTC 139.218.234.143

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../db');
 
 router.get('/dogs', async(req, res) => {
-    const [rows]= await db.query(`SHOW DATABASES;
+    const [rows]= await db.query(`SELECT * FROM Dog;
 `);
     res.json(rows);
 });
