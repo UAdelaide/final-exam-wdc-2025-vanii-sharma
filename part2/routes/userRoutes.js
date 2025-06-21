077991666a162188606580a1e68eb5cc0a206ffb
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-21 03:10:55 +0000

    [automatic commit] 2025-06-21 03:10:55 UTC 139.218.234.143

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
-const bcrypt = require('bcrypt');
 
 // GET all users (for admin/testing)
 router.get('/', async (req, res) => {
