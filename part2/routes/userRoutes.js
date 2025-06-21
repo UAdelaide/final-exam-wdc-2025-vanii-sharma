5cea0a3ae047291bc1a731c5357d5aa000f528c5
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-21 06:08:55 +0000

    [automatic commit] 2025-06-21 06:08:55 UTC 139.218.234.143

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -34,7 +34,7 @@ router.get('/me', (req, res) => {
   if (!req.session.user) {
     return res.status(401).json({ error: 'Not logged in' });
   }
-  res.json(req.session.user);
+  return res.json(req.session.user);
 });
 
 // POST login (dummy version)
