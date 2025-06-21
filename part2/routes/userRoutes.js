f5c235a2d81bd0a12a34a74de2841949a1329d8f
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-21 02:32:25 +0000

    [automatic commit] 2025-06-21 02:32:25 UTC 139.218.234.143

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -42,7 +42,7 @@ router.post('/login', async (req, res) => {
 
   try {
     const [rows] = await db.query(`
-      SELECT user_id, username, role FROM Users
+      SELECT user_id, username, password_hash, role FROM Users
       WHERE username = ? AND password_hash = ?
     `, [username]);
 
