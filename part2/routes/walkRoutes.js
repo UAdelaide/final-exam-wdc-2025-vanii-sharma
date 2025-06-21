b147c40fdd4234f5219a9da0f5a1d56bdfdb13db
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-21 05:30:49 +0000

    [automatic commit] 2025-06-21 05:30:49 UTC 139.218.234.143

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -21,7 +21,6 @@ router.get('/', async (req, res) => {
 
 // POST a new walk request (from owner)
 router.post('/', async (req, res) => {
-  const { dog_id, requested_time, duration_minutes, location } = req.body;
 
   try {
     const [result] = await db.query(`
