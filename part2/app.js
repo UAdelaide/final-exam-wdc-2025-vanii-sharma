96a6a32849ba8c1043655a5796c884ac410a81fe
vanii-sharma <a1871009@adelaide.edu.au>
2025-06-21 02:11:18 +0000

    [automatic commit] 2025-06-21 02:11:18 UTC 139.218.234.143

--- part2/app.js
+++ part2/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
-const session = 
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
